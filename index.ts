ffd7a2dc9324895ac441de9120794bc98a9b9ad5
Rudi MK <rudimk.cloud@gmail.com>
2024-11-26 08:32:59 +0000

    Added basic logging for fun

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 const server = Bun.serve({
     port: 3000,
     fetch(request) {
+      console.write(`${request.method} ${request.url}\n`)
       return Response.json({en: "Welcome to Bun & Porter!", zh: "欢迎来到 Bun & Porter!", ja: "Bun & Porterへようこそ！", th: "ยินดีต้อนรับสู่ Bun & Porter!"});
     },
   });
