a93d7839ef2f9e70f54a904929a87ac97b4d17c5
Rudi MK <rudimk.cloud@gmail.com>
2024-11-26 07:47:30 +0000

    Switched to using 3000 as the default port.

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 const server = Bun.serve({
-    port: 3839,
+    port: 3000,
     fetch(request) {
       return Response.json({en: "Welcome to Bun & Porter!", zh: "欢迎来到 Bun & Porter!", ja: "Bun & Porterへようこそ！", th: "ยินดีต้อนรับสู่ Bun & Porter!"});
     },
